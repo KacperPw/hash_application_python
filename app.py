bfdd768713bee23b184b9e5fc2e23159bd363abb
KacperPw <kpawski124@gmail.com>
2022-05-06 22:45:55 +0200

    first version

---
+++ app.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import *
+import bcrypt
+
+
+root = tk.Tk()
+root.geometry("480x480")
+
+label1 = tk.Label(root, text="Enter data to hash")
+label1.pack()
+
+entry1 = tk.Entry(root)
+entry1.pack()
+
+
+def btnfunc():
+
+    data_to_hash = entry1.get().encode("utf-8")
+
+    hashed_data = bcrypt.hashpw(data_to_hash, bcrypt.gensalt())
+
+    #hashed_data = hashlibrary(data_to_hash)
+    label2 = tk.Label(root, text=hashed_data)
+    label2.pack()
+
+
+btn1 = tk.Button(root, text="Im button", command=btnfunc)
+btn1.pack()
+
+
+root.mainloop()
